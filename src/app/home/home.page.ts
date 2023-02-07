219ad25fc48945d3f9aa3ba34cedcea8e5ac8fc8
Michelle Marques <michellea.m.silva@gmail.com>
2023-02-07 19:19:40 -0300

    Pequenos ajustes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+
+  public resultado: String = "Resultado";
+  precoAlcool!: Number;
+  precoGasolina!: Number;
+
+
+  calcular() {
+
+
+    // validação de preenchimento de campos
+    if (this.precoAlcool && this.precoGasolina) {
+
+      let pAlcool = Number(this.precoAlcool);
+      let pGasolina = Number(this.precoGasolina);
+
+      let res = pAlcool / pGasolina;
+      if(res >= 0.7) {
+        this.resultado ="Melhor utilizar Gasolina";
+      }else {
+        this.resultado ="Melhor utilizar Álcool";
+      }
+
+    } else {
+      this.resultado = "Preencha corretamente os campos";
+      
+    }
+  }
 
 }
